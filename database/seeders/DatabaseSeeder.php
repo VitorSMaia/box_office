bf5b332a743914f74f7f837efb6225c0a5417902
VitorSMaia <vitor.smaia1@gmail.com>
2023-06-09 17:31:18 -0300

    Ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(1)->create();
 
-//         User::query()->findOrFail(1)->assignRole('Administrador');
-//         \App\Models\User::factory()->create([
-//             'name' => 'Test User',
-//             'email' => 'test@example.com',
-//         ]);
+         \App\Models\User::factory()->create([
+            'name' => 'Vitor Maia',
+            'email' => 'vitor.smaia1@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123456789', // password
+            'remember_token' => Str::random(10),
+        ]);
+         User::query()->findOrFail(1)->assignRole('Administrador');
     }
 }
